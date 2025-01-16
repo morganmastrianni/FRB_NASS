5b50625c36ce747c77d1b43363c55a1c7fd31be8
morganmastrianni <morganmastrianni@gmail.com>
2025-01-16 11:23:36 -0500

    testing merge

--- nass_pull.py
+++ nass_pull.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests as req
 import os
 from dotenv import load_dotenv
@@ -16,20 +15,6 @@ rsc = "get_counts"
 # get_nass_quickstats(frb, income)
 
 data = req.get(f"{base_url}/{rsc}/?key={api_key}&commodity_desc=CORN&year__GE=2012&state_alpha=NE").text
-=======
-import requests as req
-
-base_url = "https://quickstats.nass.usda.gov/api/"
-api_key = "4719014D-12BE-3B3D-9339-6C9F991F153C"
-rsc = "get_counts"
-
-# def get_nass_quickstats(frb_dist, rsc, params):
-#     url = f"{base_url}/{rsc}/?key={api_key}&{params}"
-#     req.get(url).text
-
-# frb = "kc"
-# get_nass_quickstats(frb, income)
-
-data = req.get(f"{base_url}/{rsc}/?key={api_key}&commodity_desc=CORN&year__GE=2012&state_alpha=N").text
->>>>>>> 135a922ba53f58fbe126175dd3e543bb4437aae2
 print(data)
+
+print("test")
