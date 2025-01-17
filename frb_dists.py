76e2bb0af691d939403bcd1f72cbd8a294011883
morganmastrianni <morganmastrianni@gmail.com>
2025-01-17 13:14:15 -0500

    tuples with district, state code, and county fips

--- frb_dists.py
+++ frb_dists.py
@@ -2,12 +2,32 @@ import polars as pl
 import matplotlib.pyplot as plt
 
 df = pl.read_csv("FedCounties.csv")
-# print(df)
 
-for d in df.partition_by("District"):
-    plt.scatter(d["INTPTLON"], d["INTPTLAT"], s=2, alpha=0.5, label=d["District"][0])
-plt.xlim(-126, -67)
-plt.ylim(24, 50)
-plt.grid()
-plt.legend()
-plt.show()
+# pt map of counties by fed district
+# for d in df.partition_by("District"):
+#     plt.scatter(d["INTPTLON"], d["INTPTLAT"], s=2, alpha=0.5, label=d["District"][0])
+# plt.xlim(-126, -67)
+# plt.ylim(24, 50)
+# plt.grid()
+# plt.legend()
+# plt.show()
+tuples = []
+
+for dist in range(1, 13):
+    filtered = df.filter(df["District"] == dist)
+    tuples.extend(
+        zip(
+            filtered["District"].to_list(),
+            filtered["STATEFP"].to_list(),
+            filtered["COUNTYFP"].to_list(),
+        )
+    )
+
+district = 1
+
+j = 0
+for i in tuples:
+    if i[0] == district:
+        j += 1
+
+print(j)
--- nass_pull.py
+++ nass_pull.py
@@ -9,6 +9,26 @@ load_dotenv()
 base_url = "https://quickstats.nass.usda.gov/api/"
 api_key = os.getenv("NASS_api_key")
 # rsc = "api_GET"
+tuples = []
+
+for dist in range(1, 13):
+    filtered = df.filter(df["District"] == dist)
+    tuples.extend(
+        zip(
+            filtered["District"].to_list(),
+            filtered["STATEFP"].to_list(),
+            filtered["COUNTYFP"].to_list(),
+        )
+    )
+
+district = 1
+
+j = 0
+for i in tuples:
+    if i[0] == district:
+        j += 1
+
+print(j)
 
 # def get_nass_quickstats(frb_dist, rsc, params):
 #     url = f"{base_url}/{rsc}/?key={api_key}&{params}"
@@ -33,12 +53,10 @@ raw = req.get(
 
 # NASS accepts value=1&value=2, not value=1,2
 
-if raw.status_code==200:
+if raw.status_code == 200:
     data = raw.text
     df = pl.read_csv(StringIO(data))
     print(df)
     df.write_csv("NASS_df.csv")
 else:
     print(f"Womp womp... {raw.status_code}")
-
-
