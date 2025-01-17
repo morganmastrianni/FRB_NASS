a78a54a63c21e3a1f49e6d14c8309a4658bb245e
morganmastrianni <morganmastrianni@gmail.com>
2025-01-17 15:44:26 -0500

    zfill tuples

--- nass_pull.py
+++ nass_pull.py
@@ -9,6 +9,7 @@ load_dotenv()
 base_url = "https://quickstats.nass.usda.gov/api/"
 api_key = os.getenv("NASS_api_key")
 # rsc = "api_GET"
+df = pl.read_csv("FedCounties.csv")
 tuples = []
 
 for dist in range(1, 13):
@@ -20,43 +21,36 @@ for dist in range(1, 13):
             filtered["COUNTYFP"].to_list(),
         )
     )
+tuples = [(t[0], t[1].zfill(2), t[2].zfill(3)) for t in tuples]
 
 district = 1
-
-j = 0
-for i in tuples:
-    if i[0] == district:
-        j += 1
-
-print(j)
 
 # def get_nass_quickstats(frb_dist, rsc, params):
 #     url = f"{base_url}/{rsc}/?key={api_key}&{params}"
 #     req.get(url).text
 
 # frb = "kc"
-state = "NE"
+state = "ME"
 # get_nass_quickstats(frb, income)
 
-raw = req.get(
-    f"{base_url}/api_GET/?key={api_key}&agg_level_desc=COUNTY&commodity_desc=CATTLE&state_alpha={state}&source_desc=CENSUS&year=2022&format=CSV"
-)
+# raw = req.get(
+#     f"{base_url}/api_GET/?key={api_key}&agg_level_desc=COUNTY&commodity_desc=CATTLE&state_alpha={state}&source_desc=CENSUS&year=2022&format=CSV"
+# )
 
 # params = req.get(
 #     f"{base_url}/get_param_values/?key={api_key}&param=short_desc"
 # )
 
-# results = params.text.split("\n")
-# for line in results:
-#     if "CATTLE" in line:
-#         print(line)
+for i in tuples:
+    if i[0] == district:
 
+req.get()
 # NASS accepts value=1&value=2, not value=1,2
 
-if raw.status_code == 200:
-    data = raw.text
-    df = pl.read_csv(StringIO(data))
-    print(df)
-    df.write_csv("NASS_df.csv")
-else:
-    print(f"Womp womp... {raw.status_code}")
+# if raw.status_code == 200:
+#     data = raw.text
+#     df = pl.read_csv(StringIO(data))
+#     print(df)
+#     df.write_csv("NASS_df.csv")
+# else:
+#     print(f"Womp womp... {raw.status_code}")
