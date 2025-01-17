a7505d9167b62e4a9e883883700351afd7196b35
morganmastrianni <morganmastrianni@gmail.com>
2025-01-17 11:47:13 -0500

    figuring out data construction

--- nass_pull.py
+++ nass_pull.py
@@ -1,12 +1,14 @@
+import polars as pl
 import requests as req
 import os
 from dotenv import load_dotenv
+from io import StringIO
 # import json
 
 load_dotenv()
 base_url = "https://quickstats.nass.usda.gov/api/"
 api_key = os.getenv("NASS_api_key")
-rsc = "get_param_values"
+# rsc = "api_GET"
 
 # def get_nass_quickstats(frb_dist, rsc, params):
 #     url = f"{base_url}/{rsc}/?key={api_key}&{params}"
@@ -16,9 +18,27 @@ rsc = "get_param_values"
 state = "NE"
 # get_nass_quickstats(frb, income)
 
-data = req.get(
-    f"{base_url}/{rsc}/?key={api_key}&param=county_code&state_alpha={state}"
-).text
-print(data)
+raw = req.get(
+    f"{base_url}/api_GET/?key={api_key}&agg_level_desc=COUNTY&commodity_desc=CATTLE&state_alpha={state}&source_desc=CENSUS&year=2022&format=CSV"
+)
+
+# params = req.get(
+#     f"{base_url}/get_param_values/?key={api_key}&param=short_desc"
+# )
+
+# results = params.text.split("\n")
+# for line in results:
+#     if "CATTLE" in line:
+#         print(line)
+
+# NASS accepts value=1&value=2, not value=1,2
+
+if raw.status_code==200:
+    data = raw.text
+    df = pl.read_csv(StringIO(data))
+    print(df)
+    df.write_csv("NASS_df.csv")
+else:
+    print(f"Womp womp... {raw.status_code}")
 
 
