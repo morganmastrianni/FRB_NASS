2ba0abbb605cc462d9bb2c65e3ac2c0faf2ee0c4
morganmastrianni <morganmastrianni@gmail.com>
2025-01-18 16:23:14 -0500

    liams changes

--- nass_pull.py
+++ nass_pull.py
@@ -4,11 +4,8 @@ import os
 from dotenv import load_dotenv
 import json
 from alive_progress import alive_bar
+import csv
 
-load_dotenv()
-base_url = "https://quickstats.nass.usda.gov/api/"
-api_key = os.getenv("NASS_api_key")
-# rsc = "api_GET"
 fed_counties_df = pl.read_csv("FedCounties.csv")
 tuples = []
 
@@ -23,27 +20,20 @@ for dist in range(1, 13):
     )
 tuples = [(t[0], str(t[1]).zfill(2), str(t[2]).zfill(3)) for t in tuples]
 
-# def get_nass_quickstats(frb_dist, rsc, params):
-#     url = f"{base_url}/{rsc}/?key={api_key}&{params}"
-#     req.get(url).text
-
-# frb = "kc"
-# state = "ME"
-# get_nass_quickstats(frb, income)
-
-# raw = req.get(
-#     f"{base_url}/api_GET/?key={api_key}&agg_level_desc=COUNTY&commodity_desc=CATTLE&state_alpha={state}&source_desc=CENSUS&year=2022&format=CSV"
-# )
-
 pairs_1 = [
     (state, county) for district, state, county in tuples if district == 1
 ]
+
+load_dotenv()
+url = "https://quickstats.nass.usda.gov/api/api_GET"
+api_key = os.getenv("NASS_api_key")
+
+from time import sleep
 
 dfs = []
 with alive_bar(len(pairs_1), title='Pairs') as bar:
     for state, county in pairs_1:
         bar()
-        url = f"{base_url}api_GET"
         raw = req.get(
             url,
             params={
@@ -57,8 +47,17 @@ with alive_bar(len(pairs_1), title='Pairs') as bar:
                 "format": "json"
             },
         ).text
+        sleep(1)
 
-        content = json.loads(raw)
+        # if 'error' in content:
+        #     sleep(1)
+
+        # try:
+        #     content = json.loads(raw)
+        # except json.decoder.JSONDecodeError as e:
+        #     print(raw)
+        #     raise e
+        
         if 'error' in content:
             print(state, county)
             print(content['error'])
@@ -68,15 +67,14 @@ with alive_bar(len(pairs_1), title='Pairs') as bar:
         df = df.select([pl.col(c) for c in sorted(df.columns)])
         dfs.append(df)
 
-pl.concat(dfs).write_parquet('NASS_pull.parquet')
-
+df = pl.concat(dfs)
+df.write_parquet('NASS_pull.parquet')
+df.write_csv("NASS_pull.csv")
 
 # params = req.get(
 #     f"{base_url}/get_param_values/?key={api_key}&param=short_desc"
 # )
 
-# NASS accepts value=1&value=2, not value=1,2
-
 # if raw.status_code == 200:
 #     data = raw.text
 #     df = pl.read_csv(StringIO(data))
@@ -85,10 +83,3 @@ pl.concat(dfs).write_parquet('NASS_pull.parquet')
 # else:
 #     print(f"Womp womp... {raw.status_code}")
 
-# if raw.status_code == 200:
-#     data = raw.text
-#     df = pl.read_csv(StringIO(data))
-#     print(df)
-#     df.write_csv("NASS_df.csv")
-# else:
-#     print(f"Womp womp... {raw.status_code}")
